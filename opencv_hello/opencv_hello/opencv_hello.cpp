10ad13f6207d7da282af64ed9cf977802e99c561
junaoki <junaoki@yahoo-inc.com>
2012-08-15 16:24:06 -0700

    added face detection example

--- opencv_hello/opencv_hello/opencv_hello.cpp
+++ opencv_hello/opencv_hello/opencv_hello.cpp
@@ -1,4 +1,4 @@
-// opencv_hello.cpp : Defines the entry point for the console application.
+﻿// opencv_hello.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -7,9 +7,61 @@
 #include <cxcore.h>
 #include <highgui.h>
 
+CvHaarClassifierCascade *cascade;
+CvMemStorage            *storage;
+
+void detectFaces( IplImage *img )
+{
+    int i;
+ 
+    /* detect faces */
+    CvSeq *faces = cvHaarDetectObjects(
+            img,
+            cascade,
+            storage,
+            1.1,
+            3,
+            0 /*CV_HAAR_DO_CANNY_PRUNNING*/,
+            cvSize( 40, 40 ) );
+ 
+    /* for each face found, draw a red box */
+    for( i = 0 ; i < ( faces ? faces->total : 0 ) ; i++ ) {
+        CvRect *r = ( CvRect* )cvGetSeqElem( faces, i );
+        cvRectangle( img,
+                     cvPoint( r->x, r->y ),
+                     cvPoint( r->x + r->width, r->y + r->height ),
+                     CV_RGB( 255, 0, 0 ), 1, 8, 0 );
+    }
+ 
+    /* display video */
+    cvShowImage( "video", img );
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int type = 1;
+
+	// This block 
+			CvCapture *capture;
+		IplImage  *img = cvLoadImage("lena.jpg"); 
+		int       key;
+		char      *filename = "C:\\OpenCV2.2\\data\\haarcascades\\haarcascade_frontalface_alt.xml";
+
+		cascade = ( CvHaarClassifierCascade* )cvLoad( filename, 0, 0, 0 );
+		storage = cvCreateMemStorage( 0 );
+		// img     = cvLoadImage( "fuji.jpg", 1 ); 
+		
+
+		assert( cascade && storage && img );
+
+		cvNamedWindow( "video", 1 );
+		detectFaces( img );
+		cvWaitKey( 0 );
+		cvDestroyWindow( "video" );
+		cvReleaseImage( &img );
+		cvReleaseHaarClassifierCascade( &cascade );
+		cvReleaseMemStorage( &storage );
+
 
 	if(type == 1){ // Show some fuji.jpg pic
 		IplImage *img = cvLoadImage("fuji.jpg");
@@ -19,6 +71,82 @@ int _tmain(int argc, _TCHAR* argv[])
 		cvWaitKey();
 		cvDestroyWindow("Image:");
 		cvReleaseImage(&img);
+
+	}else if(type == 2){ // face detection example (failed???)
+
+		CvCapture *capture;
+		IplImage  *img = cvLoadImage("fuji.jpg"); 
+		int       key;
+		char      *filename = "C:\OpenCV2.2\data\haarcascades\haarcascade_frontalface_alt.xml";
+
+		cascade = ( CvHaarClassifierCascade* )cvLoad( filename, 0, 0, 0 );
+		storage = cvCreateMemStorage( 0 );
+		// img     = cvLoadImage( "fuji.jpg", 1 ); 
+		
+
+		assert( cascade && storage && img );
+
+		cvNamedWindow( "video", 1 );
+		detectFaces( img );
+		cvWaitKey( 0 );
+		cvDestroyWindow( "video" );
+		cvReleaseImage( &img );
+		cvReleaseHaarClassifierCascade( &cascade );
+		cvReleaseMemStorage( &storage );
+
+	}else if(type == 3){ // face detection example
+
+		int i;
+		IplImage *src_img = 0, *src_gray = 0;
+		const char *cascade_name = "C:\OpenCV2.2\data\haarcascades\haarcascade_frontalface_default.xml";
+		CvHaarClassifierCascade *cascade = 0;
+		CvMemStorage *storage = 0;
+		CvSeq *faces;
+		static CvScalar colors[] = {
+			{{0, 0, 255}}, {{0, 128, 255}},
+			{{0, 255, 255}}, {{0, 255, 0}},
+			{{255, 128, 0}}, {{255, 255, 0}},
+			{{255, 0, 0}}, {{255, 0, 255}}
+		};
+
+		// (1)画像を読み込む
+		if (argc < 2 || (src_img = cvLoadImage ("fuji.jpg", CV_LOAD_IMAGE_COLOR)) == 0)
+			return -1;
+		src_gray = cvCreateImage (cvGetSize (src_img), IPL_DEPTH_8U, 1);
+
+		// (2)ブーストされた分類器のカスケードを読み込む
+		cascade = (CvHaarClassifierCascade *) cvLoad (cascade_name, 0, 0, 0);
+
+		// (3)メモリを確保し，読み込んだ画像のグレースケール化，ヒストグラムの均一化を行う
+		storage = cvCreateMemStorage (0);
+		cvClearMemStorage (storage);
+		cvCvtColor (src_img, src_gray, CV_BGR2GRAY);
+		cvEqualizeHist (src_gray, src_gray);
+
+		// (4)物体（顔）検出
+		faces = cvHaarDetectObjects (src_gray, cascade, storage, 1.11, 4, 0, cvSize (40, 40));
+
+		// (5)検出された全ての顔位置に，円を描画する
+		for (i = 0; i < (faces ? faces->total : 0); i++) {
+		CvRect *r = (CvRect *) cvGetSeqElem (faces, i);
+		CvPoint center;
+		int radius;
+		center.x = cvRound (r->x + r->width * 0.5);
+		center.y = cvRound (r->y + r->height * 0.5);
+		radius = cvRound ((r->width + r->height) * 0.25);
+		cvCircle (src_img, center, radius, colors[i % 8], 3, 8, 0);
+		}
+
+		// (6)画像を表示，キーが押されたときに終了
+		cvNamedWindow ("Face Detection", CV_WINDOW_AUTOSIZE);
+		cvShowImage ("Face Detection", src_img);
+		cvWaitKey (0);
+
+		cvDestroyWindow ("Face Detection");
+		cvReleaseImage (&src_img);
+		cvReleaseImage (&src_gray);
+		cvReleaseMemStorage (&storage);
+
 	}else{ // show Hello World
 		cvNamedWindow( "My Window", 1 );
 		IplImage *img = cvCreateImage( cvSize( 640, 480 ), IPL_DEPTH_8U, 1 );
@@ -31,6 +159,11 @@ int _tmain(int argc, _TCHAR* argv[])
 		cvShowImage( "My Window", img );
 		cvWaitKey();
 	}
+
 	return 0;
+
 }
+
+
+
 
