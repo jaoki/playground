58e9429998235e4f3dd05c9f6d91220a7b4fcc55
junaoki <junaoki@yahoo-inc.com>
2012-08-15 19:12:29 -0700

    some refactoring

--- opencv_hello/opencv_hello/opencv_hello.cpp
+++ opencv_hello/opencv_hello/opencv_hello.cpp
@@ -13,29 +13,23 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	IplImage *jpgimg = cvLoadImage("lena.jpg"); 
 	const char *haarFile = "C:\\OpenCV2.2\\data\\haarcascades\\haarcascade_frontalface_alt.xml";
+	CvHaarClassifierCascade *cascade = ( CvHaarClassifierCascade* )cvLoad( haarFile, 0, 0, 0 );
+	cvNamedWindow("opencv test", CV_WINDOW_NORMAL);
 
 	if(type == 1){ // Show some jpg pic
-		cvNamedWindow("Image:",1);
-		cvShowImage("Image:",jpgimg);
-
+		cvShowImage("opencv test",jpgimg);
 		cvWaitKey();
-		cvDestroyWindow("Image:");
 
 	}else if(type == 2){ // face detection example (failed???)
 
 		CvCapture *capture;
 		int       key;
-
-		CvHaarClassifierCascade *cascade = ( CvHaarClassifierCascade* )cvLoad( haarFile, 0, 0, 0 );
 		CvMemStorage *storage = cvCreateMemStorage( 0 );
-		// jpgimg     = cvLoadImage( "fuji.jpg", 1 ); 
 		
 
 		assert( cascade && storage && jpgimg );
 
 		cvNamedWindow( "video", 1 );
-//		detectFaces( jpgimg );
-
 	 
 		/* detect faces */
 		CvSeq *faces = cvHaarDetectObjects(
@@ -60,18 +54,14 @@ int _tmain(int argc, _TCHAR* argv[])
 		/* display video */
 		cvShowImage( "video", jpgimg );
 
-
-
 		cvWaitKey( 0 );
 		cvDestroyWindow( "video" );
-		cvReleaseHaarClassifierCascade( &cascade );
 		cvReleaseMemStorage( &storage );
 
 	}else if(type == 3){ // face detection example
 
 		int i;
 	   	IplImage *src_gray = 0;
-		CvHaarClassifierCascade *cascade = 0;
 		CvMemStorage *storage = 0;
 		CvSeq *faces;
 		static CvScalar colors[] = {
@@ -83,9 +73,6 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		// (1)画像を読み込む
 		src_gray = cvCreateImage (cvGetSize (jpgimg), IPL_DEPTH_8U, 1);
-
-		// (2)ブーストされた分類器のカスケードを読み込む
-		cascade = (CvHaarClassifierCascade *) cvLoad (haarFile, 0, 0, 0);
 
 		// (3)メモリを確保し，読み込んだ画像のグレースケール化，ヒストグラムの均一化を行う
 		storage = cvCreateMemStorage (0);
@@ -108,17 +95,17 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 
 		// (6)画像を表示，キーが押されたときに終了
-		cvNamedWindow ("Face Detection", CV_WINDOW_AUTOSIZE);
-		cvShowImage ("Face Detection", jpgimg);
+		// cvNamedWindow ("Face Detection", CV_WINDOW_AUTOSIZE);
+//		cvNamedWindow ("Face Detection", CV_WINDOW_NORMAL);
+		cvShowImage ("opencv test", jpgimg);
 		cvWaitKey (0);
 
-		cvDestroyWindow ("Face Detection");
-//		cvReleaseImage (&jpgimg);
+		// cvDestroyWindow ("Face Detection");
 		cvReleaseImage (&src_gray);
 		cvReleaseMemStorage (&storage);
 
 	}else{ // show Hello World
-		cvNamedWindow( "My Window", 1 );
+//		cvNamedWindow( "My Window", 1 );
 		IplImage *newimg = cvCreateImage( cvSize( 640, 480 ), IPL_DEPTH_8U, 1 );
 		CvFont font;
 		double hScale = 2.0;
@@ -126,11 +113,13 @@ int _tmain(int argc, _TCHAR* argv[])
 		int lineWidth = 5;
 		cvInitFont(&font, CV_FONT_HERSHEY_SIMPLEX | CV_FONT_ITALIC, hScale, vScale, 0, lineWidth);
 		cvPutText(newimg, "Hello World!", cvPoint( 100, 200 ), &font, cvScalar( 255, 255, 0 ) );
-		cvShowImage( "My Window", newimg );
+		cvShowImage( "opencv test", newimg );
 		cvWaitKey();
 	}
 
+	cvDestroyWindow ("opencv test");
 	cvReleaseImage(&jpgimg);
+	cvReleaseHaarClassifierCascade( &cascade );
 	
 	return 0;
 
