21c2f85b6ab7837db59e4ab9a3ffb406c15d50cc
jun aoki <jun.aoki.0713@gmail.com>
2012-08-30 01:09:40 -0700

    modified js a bit

--- bookmarklet/public/javascripts/test.js
+++ bookmarklet/public/javascripts/test.js
@@ -1,5 +1,11 @@
 (function(){
-	window.location
-	alert("aaa");
+	var body = document.getElementsByTagName('body') [0];
+	var divGray = document.createElement('div');
+	var windowHeight = window.innerHeight;
+	var windowWidth = window.innerWidth;
+	divGray.setAttribute("style","background-color: gray; height:" + windowHeight + "px;width:" + windowWidth + "px;");
+
+	body.appendChild(divGray);
+	alert("aaab");
 })()
 
